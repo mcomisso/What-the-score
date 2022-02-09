c7248e5040e586cc86bc2f6c87d8d644fdcd21ab
Matteo Comisso <matteo.comisso@me.com>
2022-02-09 10:23:38 +0000

    Initial Commit

---
+++ ScoreMatching/ScoreMatchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreMatchingApp.swift
+//  ScoreMatching
+//
+//  Created by Matteo Comisso on 09/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ScoreMatchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
