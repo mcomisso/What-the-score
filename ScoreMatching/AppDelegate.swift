fed302525afa254176f70688ccf894b62fa7095f
Matteo Comisso <matteo.comisso@me.com>
2022-12-22 19:24:53 +0000

    Updated to 1.2.0

--- ScoreMatching/AppDelegate.swift
+++ ScoreMatching/AppDelegate.swift
@@ -1,13 +1,26 @@
 import Foundation
-import FirebaseCore
+import TelemetryClient
 import UIKit
 
+private extension Bundle {
+    var analyticsID: String {
+        let id = infoDictionary?["AnalyticsID"] as? String
+        return id!
+    }
+}
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        #if !DEBUG
-        FirebaseApp.configure()
-        #endif
+
+        let configuration = TelemetryManagerConfiguration(appID: Bundle.main.analyticsID)
+        TelemetryManager.initialize(with: configuration)
+
         return true
+    }
+}
+
+class Analytics {
+    static func log(_ event: String) {
+        TelemetryManager.send(event)
     }
 }
